dfbf327716c6abe3d6966c27f74ac5509a2d5f2d
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-09-14 21:12:21 +0500

    Done

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-red-500">
+      <Text className="text-white">Open up App.js to start working on your app!</Text>
     </View>
   );
 }
