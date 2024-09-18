5eca1a3323c4b55747261ecc2c9ff5616149583d
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-09-18 23:25:17 +0500

    Done

--- Apps/screens/Itemlist.jsx
+++ Apps/screens/Itemlist.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
@@ -9,6 +9,7 @@ export default function Itemlist() {
   const {params} = useRoute()
   const db = getFirestore(app)
   const [itemlist, setItemlist] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(()=>{
    params&&getItemlistByCategory()
@@ -16,21 +17,27 @@ export default function Itemlist() {
 
   const getItemlistByCategory = async ()=>{
     setItemlist([])
+    setLoading(true)
     const q = query(collection(db, "UserPost"), where("category", "==", params.category));
     const querySnapshot = await getDocs(q);
+    setLoading(false)
     querySnapshot.forEach((doc) => {
       console.log(doc.id, " => ", doc.data());
       setItemlist(itemlist => [...itemlist, doc.data()])
+      setLoading(false)
     });
   }
   return (
     <View className='p-2'>
       {
+        loading ? <ActivityIndicator className='mt-24' color={'#3b82f6'}/>
+        :
         itemlist?.length > 0 ? <LatestItem latestItem={itemlist}
         heading={''}
         />
   : <Text className='p-5 mt-2 text-[20px] font-bold text-center text-gray-500 justify-center items-center'>No item found</Text>  
       }
+      
     </View>
   )
 }
--- Apps/screens/ProductDetail.jsx
+++ Apps/screens/ProductDetail.jsx
@@ -1,15 +1,42 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Linking, Share } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { ScrollView } from 'react-native-gesture-handler'
-
-export default function ProductDetail() {
+import Ionicons from '@expo/vector-icons/Ionicons';
+export default function ProductDetail({navigation}) {
     const {params} = useRoute()
     const [product,setProduct] = useState([])
 
     useEffect(()=>{
      params&&setProduct(params.product)
-    },[params])
+     ShareButton()
+    },[params,navigation])
+
+    const ShareButton = () => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => shareProduct()}>
+          <Ionicons name="share-social" size={24} color="white" style={{marginRight: 15}}/>
+        </TouchableOpacity>
+      ),
+    });
+    }
+
+    const shareProduct = async () => {
+      const content = {
+        message: product.title+"\n"+product.desc,
+      }
+      Share.share(content).then((result) => {
+        console.log(result)
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    const sendEmailmessage = () => {
+      const subject = `Regarding: ${product.title}`
+      const body = `${product.userName} I am interested in your product`
+      Linking.openURL(`mailto:${product.userEmail}?subject=${subject}&body=${body}`)
+    }
   return (
     <ScrollView className='bg-white'>
         <Image source={{uri:product.image}}
@@ -34,6 +61,11 @@ export default function ProductDetail() {
           <Text className='text-gray-500'>{product.userEmail}</Text>
         </View>
         </View>
+        <TouchableOpacity className='z-40 bg-blue-500 rounded-full p-3 m-2' 
+        onPress={()=>sendEmailmessage()}
+        >
+          <Text className='text-center text-white'>Send Message</Text>
+        </TouchableOpacity>
     </ScrollView>
   )
 }
